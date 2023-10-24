c7d50939e0572f5a87855272f90710ed74d84412
Charlie Ryan <cryan@Cryan-MacBook-Pro.local>
2023-10-24 13:18:01 -0400

    ex33 study drill additions

--- ex33.py
+++ ex33.py
@@ -1,18 +1,15 @@
 input_ = ('> ')
-
 print("How many numbers would you like to add?")
 num_var = input(input_)
 
-def while_loop(nums):
-	while i < num_var:
+def while_loop(num_var):
+	numbers = []
+	i = 0
+	while i < int(num_var):
 		print(f"At the top i is {i}")
 		numbers.append(i)
-
 		i = i + 1
 		print("Numbers now: ", numbers)
 		print(f"At the bottom i is {i}")
 
-
-print("The numbers: ")
-
-print(f"{while_loop}")
+print(while_loop(num_var))
