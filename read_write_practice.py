8feab8a76ec2b34202d4f2f80a2baddc8300655c
Charlie Ryan <cryan@Cryan-MacBook-Pro.local>
2023-10-27 16:23:18 -0400

    adding r/w functionality

--- read_write_practice.py
+++ read_write_practice.py
@@ -6,15 +6,27 @@ input__ = input("Would you like to read or write this file? (r/w): ")
 
 #open filename based on input
 #open mode based on input (read/write)
-with open(input_, mode=input__) as csv_file:
-	#read csv file
-	csv_reader = csv.DictReader(csv_file)
-	line = 0
-	#look at rows and if row is 0, list column names and generate column names
-	for row in csv_reader:
-		if line == 0:
-			column_names = ", ".join(row)
-			print(f"Column names are [{column_names}]")
-			line +=1
-		else:
-			print(row)
+if input__ == 'r':
+	with open(input_, mode=input__) as csv_file:
+		#read csv file
+		csv_reader = csv.DictReader(csv_file)
+		line = 0
+		#look at rows and if row is 0, list column names and generate column names
+		for row in csv_reader:
+			if line == 0:
+				column_names = ", ".join(row)
+				print(f"Column names are [{column_names}]")
+				line +=1
+			else:
+				print(row)
+elif input__ == 'w':
+	with open(input_, mode=input__) as csv_file:
+		#read csv file
+		csv_reader = csv.DictReader(csv_file)
+		line = 0
+		#look at rows and if row is 0, list column names and generate column names
+		for row in csv_reader:
+			if line == 0:
+				column_names = ", ".join(row)
+				print(f"Column names are [{column_names}]")
+				line +=1
