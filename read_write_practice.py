b8d47f1b4069d1a02f2697b556ccaa5ca6e771c6
Charlie Ryan <cryan@Cryan-MacBook-Pro.local>
2023-10-27 14:11:32 -0400

    read_write_practice.py added #descriptions to first section

--- read_write_practice.py
+++ read_write_practice.py
@@ -1,11 +1,16 @@
 import csv
 
+#input lines
 input_ = input("Input your filename: ")
 input__ = input("Would you like to read or write this file? (r/w): ")
 
-with open(input, mode=input__) as csv_file:
+#open filename based on input
+#open mode based on input (read/write)
+with open(input_, mode=input__) as csv_file:
+	#read csv file
 	csv_reader = csv.DictReader(csv_file)
 	line = 0
+	#look at rows and if row is 0, list column names and generate column field
 	for row in csv_reader:
 		if line == 0:
 			print(f"Column names are{", ".join(row)}")
