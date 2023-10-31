65da44cec192361ce24982ce49b40543c126b477
Charlie Ryan <cryan@Cryan-MacBook-Pro.local>
2023-10-31 14:49:30 -0400

    reader reformat and paired document

--- reformat.py
+++ reformat.py
@@ -1,16 +1,23 @@
 import csv
 
-input1 = input("Input your filename: ")
+input1 = 'test.csv'
 
-with open(input1, mode='r') as csv_file:
-		#read csv file
+market_data = {} #generate dictionary for market data
+
+with open(input1, mode='r', encoding='utf-8-sig') as csv_file:
 		csv_reader = csv.DictReader(csv_file)
-		line = 0
-		#look at rows and if row is 0, list column names and generate column names
-		for row in csv_reader:
-			if line == 0:
-				column_names = ", ".join(row)
-				print(f"Column names are [{column_names}]")
-				line +=1
-			else:
-				print(row)
+		csv_list = list(csv_reader)
+		
+		for row in csv_list:
+			market = row['Market']							#define market as any row with 'market' as header
+			cost = round(float(row['Cost']))				#define cost as any row with 'cost' as header
+			impressions = round(float(row['Impressions']))	#define impressions as any row with 'impressions' as header
+
+			if market not in market_data:									#if market not found in market data
+				market_data[market] = {'Cost': 0.0, 'Impressions': 0.0}		#generate parameters for market dictionary for each market
+
+			market_data[market]['Cost'] += cost					#add cost to each market every time its found in list	
+			market_data[market]['Impressions'] += impressions	#add impressions to each market every time its found in list
+		
+for market, data in market_data.items():	#list unique markets with corresponding data
+	print(f"Market: {market}\nTotal Cost: {data['Cost']}\nTotal Impressions: {data['Impressions']}\n**************")
